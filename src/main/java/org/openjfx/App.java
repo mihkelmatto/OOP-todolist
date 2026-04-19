c69fe0aa0a75b631c6f2cd7fa4f556a9b4494c35
mihkelmatto <211209697+mihkelmatto@users.noreply.github.com>
2026-04-19 10:26:05 +0300

    tõstan PC-sse lihtsalt

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -5,6 +5,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.openjfx.UUIDv7;
+
 
 /**
  * JavaFX App
@@ -13,13 +15,13 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) {
-        var javaVersion = SystemInfo.javaVersion();
-        var javafxVersion = SystemInfo.javafxVersion();
 
-        var label = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
+        var label = new Label("Hello, JavaFX ");
         var scene = new Scene(new StackPane(label), 640, 480);
         stage.setScene(scene);
         stage.show();
+        var uuid = UUIDv7.randomUUID();
+        System.out.println(uuid);
     }
 
     public static void main(String[] args) {
